8de653552db5dc1e37e1c0e843cf4d0ef6427ac1
liruihan16 <56287198+liruihan16@users.noreply.github.com>
2019-10-09 10:25:12 +0800

    Create teat1.py

---
+++ teat1.py
@@ -0,0 +1,11 @@
+import math
+a=float(input("a="))
+b=float(input("b="))
+c=float(input("c="))
+if b*b-4*a*c<0 :
+    print("wrong")
+elif b*b-4*a*c==0 :
+    print(-0.5*b/a)
+else :
+    print((-b+math.sqrt(b*b-4*a*c))/(2*a))
+    print((-b-math.sqrt(b*b-4*a*c))/(2*a))
